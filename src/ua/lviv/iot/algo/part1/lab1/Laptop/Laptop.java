8c285702363607ff2b3b5be75960705940f21782
Віктор Ясінь <122377246+YasinViktor@users.noreply.github.com>
2023-04-04 23:28:24 +0300

    Redid the old code

--- src/ua/lviv/iot/algo/part1/lab1/Laptop/Laptop.java
+++ src/ua/lviv/iot/algo/part1/lab1/Laptop/Laptop.java
@@ -1,33 +1,40 @@
 package ua.lviv.iot.algo.part1.lab1.Laptop;
-import lombok.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 @ToString
-
-
 public class Laptop {
-    private String model;
-    private double screenSize;
-    private int ram;
-    private int storage;
-    private int batteryLife;
-    private int batteryLevel;
+    private static Laptop instance = new Laptop();
+    private String model = "Macbook";
+    private double screenSize = 16.2;
+    private int ram = 8;
+    private int storage = 315;
+    private int batteryLife = 85;
+    private int batteryLevel = 91;
 
-
-
-    public Laptop() {
-        model = "Unknown";
-        screenSize = 15.6;
-        ram = 8;
-        storage = 256;
-        batteryLife = 5;
-        batteryLevel = 100;
-    }
-    private static Laptop instance=new Laptop();
-    private static Laptop getInstance(){
+    private static Laptop getInstance() {
         return instance;
     }
+
+    public static void main(String[] Args) {
+        Laptop[] laptops = new Laptop[4];
+        laptops[0] = new Laptop("Macbook", 16.2, 8, 315, 85,91);
+        laptops[1] = new Laptop();
+        laptops[2] = instance.getInstance();
+        laptops[3] = instance.getInstance();
+        for (Laptop x : laptops){
+            System.out.println(x);
+        }
+    }
+
     public void addRam(int value) {
         ram += value;
     }
@@ -39,13 +46,4 @@ public class Laptop {
     public void charge() {
         batteryLevel = 100;
     }
-    public static void main(String[] args) {
-        Laptop[] windows=new Laptop[6];
-        windows[0]=new Laptop("Macbook", 16.2, 8,315, 85, 91);
-        windows[1]=instance.getInstance();
-        windows[2]=instance.getInstance();
-        System.out.println(windows[0]);
-        System.out.println(windows[1]);
-    }
 }
-
